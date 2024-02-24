ee449f4eb11b2e8c4229589b0a1c3d5bb8a598a9
VISHESH PATEL1712 <visheshp39@gmail.com>
2024-02-24 16:13:47 +0530

    Updated 2

--- src/TicketDiscountCalculator.java
+++ src/TicketDiscountCalculator.java
@@ -3,11 +3,22 @@ public class TicketDiscountCalculator {
     public static void main(String[] args) {
         System.out.println("Welcome to Ticket Discount Calculator");
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your gender : ");
-        String gender = input.nextLine();
-        if( input.nextLine() == "Male"){
+        System.out.println("Enter your age : ");
+        int age = input.nextInt();
+        System.out.println("Are you a female (true/false) : ");
+        boolean isFemale = input.nextBoolean();
 
+        if( age < 5){
+            System.out.println("You got 75% discount");
         }
-
+        else if (isFemale){
+            System.out.println("You got 50% discount");
+        }
+        else if ( age > 60 && !isFemale){
+            System.out.println("You got 25% discount");
+        }
+        else{
+            System.out.println("You got no discount");
+        }
     }
 }
